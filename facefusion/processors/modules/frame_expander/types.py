97f726fa1604d948abfdfbe965e65fc2854bfdd3
yuri-schmaltz <yuri.schmaltz@gmail.com>
2026-01-28 15:20:21 -0300

    refactor: Replace `VisionMask` type with `Mask` type in frame expander inputs.

--- facefusion/processors/modules/frame_expander/types.py
+++ facefusion/processors/modules/frame_expander/types.py
@@ -1,10 +1,10 @@
 from typing import TypedDict, Literal
 
-from facefusion.types import VisionFrame, VisionMask
+from facefusion.types import Mask, VisionFrame
 
 FrameExpanderMode = Literal['span', 'mirror', 'reflect', 'black', 'white']
 FrameExpanderTargetRatio = Literal['16:9', '21:9', '4:3', '1:1', '9:16']
 
 class FrameExpanderInputs(TypedDict):
     temp_vision_frame : VisionFrame
-    temp_vision_mask : VisionMask
+    temp_vision_mask : Mask
